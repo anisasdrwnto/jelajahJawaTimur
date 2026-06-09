194d3f2753424c4460cfd53e4cfc0de10dc518c0
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-06-09 15:07:37 +0700

    add fitur landing page dengan nama file home

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,3 +29,6 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 //register
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.post');
+
+//Route Home
+Route::get('/home', [HomeController::class, 'index'])->name('home');
